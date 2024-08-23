468366c51724f7d22831b8d4c2007dce4eac9852
vidyanandk <vidyanandkumar200502@gmail.com>
2024-08-24 01:07:35 +0530

    chore: Update react-hook-form dependency to version 7.52.2

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,29 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm} from "react-hook-form";
+
 function Login() {
+  // useForm hook to register the form and handle the form submission
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit=(data) => console.log(data);
+
   return (
     <div>
       {/* take daisyUI modal */}
       <dialog id="my_modal_3" className="modal">
         <div className="modal-box">
-          <form method="dialog">
+          <form onSubmit={handleSubmit(onSubmit) }method="dialog" >
             {/* if there is a button in form, it will close the modal */}
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            <Link
+              to="/"
+              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+              onClick={() => document.getElementById("my_modal_3").close()}
+            >
               ✕
-            </button>
-          </form>
+            </Link>
           <h3 className="font-bold text-lg">Login</h3>
           {/* Email */}
             <div className="mt-4 space-y-2">
                <span>Email</span>
                <br />
-                <input type="email" className="w-80 px-3 rounded-md outline-none " placeholder="Enter your email" />
+                <input type="email" className="w-80 px-3 rounded-md outline-none " placeholder="Enter your email" 
+                {...register("email", { required: true })}
+                />
+                <br />
+                {/* errors will return when field validation fails  */}
+      {errors.email && (<span className="text-sm text-red-500">This field is required</span>)}
             </div>
             {/* password */}
             <div className="mt-4 space-y-2">
                <span>Password</span>
                <br />
-                <input type="text" className="w-80 px-3 rounded-md outline-none " placeholder="Enter your Password" />
+                <input type="password" className="w-80 px-3 rounded-md outline-none " placeholder="Enter your Password" 
+                {...register("password", { required: true })}
+                />
+                <br />
+                {/* errors will return when field validation fails  */}
+      {errors.password && (<span className="text-sm text-red-500">This field is required</span>)}
             </div>
             <div className="flex justify-around mt-4">
               <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">login</button>
@@ -31,6 +54,7 @@ function Login() {
                 Not registered yet? {" "}<Link to="/signup" className="underline text-blue-500 cursor-pointer">SignUp</Link>{" "}
               </p>
             </div>
+            </form>
         </div>
       </dialog>
     </div>
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Login from "./Login";
+import { useForm} from "react-hook-form";
+
 function Signup() {
+  // useForm hook to register the form and handle the form submission
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit=(data) => console.log(data);
+
   return (
     <>
       <div className="flex h-screen items-center justify-center ">
         {/* take daisyUI modal */}
         <div className="w-[600px]">
           <div className="modal-box">
-            <form method="dialog">
+            <form onSubmit={handleSubmit(onSubmit) }  method="dialog">
               {/* if there is a button in form, it will close the modal */}
               <Link  to="/" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
                 ✕
               </Link>
-            </form>
             <h3 className="font-bold text-lg">Signup</h3>
             {/* Email */}
             <div className="mt-4 space-y-2">
@@ -23,7 +32,11 @@ function Signup() {
                 type="text"
                 className="w-80 px-3 rounded-md outline-none "
                 placeholder="Enter your name"
+                {...register("name", { required: true })}
               />
+               <br />
+                {/* errors will return when field validation fails  */}
+      {errors.name && (<span className="text-sm text-red-500">This field is required</span>)}
             </div>
             {/* Email */}
             <div className="mt-4 space-y-2">
@@ -33,17 +46,25 @@ function Signup() {
                 type="email"
                 className="w-80 px-3 rounded-md outline-none "
                 placeholder="Enter your email"
+                {...register("email", { required: true })}
               />
+               <br />
+                {/* errors will return when field validation fails  */}
+      {errors.email && (<span className="text-sm text-red-500">This field is required</span>)}
             </div>
             {/* password */}
             <div className="mt-4 space-y-2">
               <span>Password</span>
               <br />
               <input
-                type="text"
+                type="password"
                 className="w-80 px-3 rounded-md outline-none "
                 placeholder="Enter your Password"
+                {...register("password", { required: true })}
               />
+               <br />
+                {/* errors will return when field validation fails  */}
+      {errors.password && (<span className="text-sm text-red-500">This field is required</span>)}
             </div>
             <div className="flex justify-around mt-4">
               <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
@@ -60,6 +81,7 @@ function Signup() {
                 <Login />
               </p>
             </div>
+            </form>
           </div>
         </div>
       </div>
